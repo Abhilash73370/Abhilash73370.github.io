6a4ea22f0a4c88826306a5f56df6b5861a01b916
Abhilash73370 <abhilashpodishetty0705@gmail.com>
2025-06-05 19:35:46 +0530

    new

--- p2/script.js
+++ p2/script.js
@@ -4,7 +4,7 @@ const showLogin = () => {
     <h1>Login Form</h1>
     <p><input type="text" id="txtEmail"></p>
     <p><input type="password" id="txtPass"></p>
-    <p><button>Log In</button></p>
+    <p><button onclick ='showHome()'>Log In</button></p>
     <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
     `
@@ -21,5 +21,13 @@ const showRegister = () => {
     <button>Register</button>
     <p><button onclick='showLogin()'>Already have an Account</button></p>
     `
+    root.innerHTML = str
+}
+const showHome = () =>{
+    let str = `
+    <h1>Welcome</h1>
+    `
+    
+    
     root.innerHTML = str
 }
